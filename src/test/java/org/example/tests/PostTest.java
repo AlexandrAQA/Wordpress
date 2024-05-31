7fa392bab2dc71c830c5ba7794da4e234a6c7e44
aqaacc <mralextb@gmail.com>
2024-05-31 13:24:04 +0200

    WordPress_diploma: first draft version with minor updates

--- src/test/java/org/example/tests/PostTest.java
+++ src/test/java/org/example/tests/PostTest.java
@@ -5,13 +5,13 @@ import org.testng.annotations.Test;
 public class PostTest extends BaseTest{
 
     @Test
-    public void userShouldCreatePost2() {
+    public void userShouldCreatePost() {
         logger.debug("Test: user Should Create Post: Opening DashboardPage.");
         loginPage.openLoginPage(url);
         loginPage.login(username, password);
         postsPage.openPostPageAndClickOnCreatingPost();
-        postsPage.createPost2("1st Test title", "Try it again. Fail again. Fail better.");
+        postsPage.createNewPost("1st Test title", "Try it again. Fail again. Fail better.");
 
-        // assertTrue(postsPage.isPostCreated(), "Post is not created");
+       // assertTrue(postsPage.isPostCreated(), "Post is not created");
     }
 }
